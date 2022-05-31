c88224bfa2a64bb634e998a674d0f331fe80d4ed
Jonathan Toye <jontoye@gmail.com>
2022-05-31 16:25:24 -0400

    check in

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle='About Me'>
+      <p>Hi there! I'm Jon - a web dev learning Gatsby!</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
